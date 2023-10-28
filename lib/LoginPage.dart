053cb5b1b4f1a4062eae32411521c867354c925c
Mohammed707se <zexr2009@gmail.com>
2023-10-28 15:53:24 +0300

    j
    j

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -158,7 +158,7 @@ class _LoginPageState extends State<LoginPage> {
                         color: Colors.black,
                       ),
                       children: <TextSpan>[
-                        TextSpan(text: "Don't have an account? "),
+                        TextSpan(text: "Don't have an Account? "),
                         TextSpan(
                             text: 'Sign Up',
                             style: TextStyle(
